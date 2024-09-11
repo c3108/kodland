54b142bbfaff34aa0044b9aa8cd0487d67ad9ccf
c3108 <chamego7878@gmail.com>
2024-09-11 20:14:38 +0700

    Update and rename dict to dict.py

--- dict.py
+++ dict.py
@@ -1,5 +1,9 @@
 meme_dict = {"CRINGE": "Sesuatu yang sangat aneh atau memalukan.",
-            "LOL": "Tanggapan umum terhadap sesuatu yang lucu.",}
+            "LOL": "Tanggapan umum terhadap sesuatu yang lucu.",
+            "ROFL": "Tanggapan terhadap lelucon",
+            "SHEESH": "Sedikit ketidaksetujuan",
+            "CREEPY": "Menakutkan, tidak menyenangkan",
+            "AGGRO": "Untuk menjadi agresif/marah",}
 
 word = input("Ketik kata yang tidak Kamu mengerti (gunakan huruf kapital semua!): ")
 
